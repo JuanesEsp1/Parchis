d1b9b30772fe31e9d74587f2eb56d67d1f991377
Sebinhas <jsebascuervo05@gmail.com>
2024-01-02 22:18:38 -0500

    JS agregue middle rows

--- components/rowsMiddle.php
+++ components/rowsMiddle.php
@@ -1,8 +1,31 @@
-<div class="row-middle">
+<div class="row-middle row-middle-player1">
     <div class="field-triangular1">
-
     </div>
-    <div class="field-triangular2">
-        
+
+    <div class="field-triangular2">   
+    </div>
+</div>
+
+<div class="row-middle row-middle-player2">
+    <div class="field-triangular1">
+    </div>
+
+    <div class="field-triangular2">   
+    </div>
+</div>
+
+<div class="row-middle row-middle-player3">
+    <div class="field-triangular1">
+    </div>
+
+    <div class="field-triangular2">   
+    </div>
+</div>
+
+<div class="row-middle row-middle-player4">
+    <div class="field-triangular1">
+    </div>
+
+    <div class="field-triangular2">   
     </div>
 </div>
