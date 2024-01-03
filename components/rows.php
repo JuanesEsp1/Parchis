c732ebbe8bfc27e8cffa273294579b8c9d1e81dc
JuanesEsp1 <juanes.espinosa01@gmail.com>
2024-01-02 20:56:34 -0500

    JE rows

---
+++ components/rows.php
@@ -0,0 +1,39 @@
+<div class="row-base row-base-player1">
+    <div class="field"></div>
+    <div class="field"></div>
+    <div class="field"></div>
+    <div class="field"></div>
+    <div class="field field-player1"></div>
+    <div class="field"></div>
+    <div class="field"></div>
+</div>
+
+<div class="row-base row-base-player2">
+    <div class="field"></div>
+    <div class="field"></div>
+    <div class="field"></div>
+    <div class="field"></div>
+    <div class="field field-player2"></div>
+    <div class="field"></div>
+    <div class="field"></div>
+</div>
+
+<div class="row-base row-base-player3">
+    <div class="field"></div>
+    <div class="field"></div>
+    <div class="field"></div>
+    <div class="field"></div>
+    <div class="field field-player3"></div>
+    <div class="field"></div>
+    <div class="field"></div>
+</div>
+
+<div class="row-base row-base-player4">
+    <div class="field"></div>
+    <div class="field"></div>
+    <div class="field"></div>
+    <div class="field"></div>
+    <div class="field field-player4"></div>
+    <div class="field"></div>
+    <div class="field"></div>
+</div>
--- components/table.php
+++ components/table.php
@@ -2,6 +2,7 @@
 
 <?php
 require("spots.php");
+require("rows.php");
 ?>
 
 
