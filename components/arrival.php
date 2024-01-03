babc22c04e381e3669b7c14daa9a11ea2d6621fa
JuanesEsp1 <juanes.espinosa01@gmail.com>
2024-01-02 23:12:12 -0500

    JE arrival

--- components/arrival.php
+++ components/arrival.php
@@ -1,3 +1,6 @@
 <div class="arrival">
-
+    <div class="arrival-player1"></div>
+    <div class="arrival-player2"></div>
+    <div class="arrival-player3"></div>
+    <div class="arrival-player4"></div>
 </div>
