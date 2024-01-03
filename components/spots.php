804f2aff15fcd5292b335059a844e0808ecfe9f8
JuanesEsp1 <juanes.espinosa01@gmail.com>
2024-01-02 20:28:21 -0500

    JE spot-container

--- components/spots.php
+++ components/spots.php
@@ -1,15 +1,23 @@
 <div class="spot spot-player1">
+    <div class="spot-container">
 
+    </div>
 </div>
 
 <div class="spot spot-player2">
-
+    <div class="spot-container">
+        
+    </div>
 </div>
 
 <div class="spot spot-player3">
-
+    <div class="spot-container">
+        
+    </div>
 </div>
 
 <div class="spot spot-player4">
-
+    <div class="spot-container">
+        
+    </div>
 </div>
