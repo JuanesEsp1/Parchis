d49e019db442b480932a162e3bcd7c9548049fdb
JuanesEsp1 <juanes.espinosa01@gmail.com>
2024-01-05 22:12:30 -0500

    JE atributos

--- components/table/rowsSecond.php
+++ components/table/rowsSecond.php
@@ -1,39 +1,39 @@
 <div class="row-base row-second-player1">
-    <div id="field12" class="field"><div class="rotate-90">12</div></div>
-    <div id="field11" class="field"><div class="rotate-90">11</div></div>
-    <div id="field10" class="field"><div class="rotate-90">10</div></div>
-    <div id="field9" class="field"><div class="rotate-90">9</div></div>
-    <div id="field8" class="field field-security"><div class="rotate-90">8</div></div>
-    <div id="field7" class="field"><div class="rotate-90">7</div></div>
-    <div id="field6" class="field"><div class="rotate-90">6</div></div>
+    <div id="field12" player2="" player3="" player4="" class="field"><div class="rotate-90">12</div></div>
+    <div id="field11" player2="" player3="" player4="" class="field"><div class="rotate-90">11</div></div>
+    <div id="field10" player2="" player3="" player4="" class="field"><div class="rotate-90">10</div></div>
+    <div id="field9" player2="" player3="" player4="" class="field"><div class="rotate-90">9</div></div>
+    <div id="field8" player2="" player3="" player4="" class="field field-security"><div class="rotate-90">8</div></div>
+    <div id="field7" player2="" player3="" player4="" class="field"><div class="rotate-90">7</div></div>
+    <div id="field6" player2="" player3="" player4="" class="field"><div class="rotate-90">6</div></div>
 </div>
 
 <div class="row-base row-second-player2">
-    <div id="field63" class="field">63</div>
-    <div id="field62" class="field">62</div>
-    <div id="field61" class="field">61</div>
-    <div id="field60" class="field">60</div>
-    <div id="field59" class="field field-security">59</div>
-    <div id="field58" class="field">58</div>
-    <div id="field57" class="field">57</div>
+    <div id="field63" player2="" player3="" player4="" class="field">63</div>
+    <div id="field62" player2="" player3="" player4="" class="field">62</div>
+    <div id="field61" player2="" player3="" player4="" class="field">61</div>
+    <div id="field60" player2="" player3="" player4="" class="field">60</div>
+    <div id="field59" player2="" player3="" player4="" class="field field-security">59</div>
+    <div id="field58" player2="" player3="" player4="" class="field">58</div>
+    <div id="field57" player2="" player3="" player4="" class="field">57</div>
 </div>
 
 <div class="row-base row-second-player3">
-    <div id="field46" class="field"><div class="rotate">46</div></div>
-    <div id="field45" class="field"><div class="rotate">45</div></div>
-    <div id="field44" class="field"><div class="rotate">44</div></div>
-    <div id="field43" class="field"><div class="rotate">43</div></div>
-    <div id="field42" class="field field-security"><div class="rotate">42</div></div>
-    <div id="field41" class="field"><div class="rotate">41</div></div>
-    <div id="field40" class="field"><div class="rotate">40</div></div>
+    <div id="field46" player2="" player3="" player4="" class="field"><div class="rotate">46</div></div>
+    <div id="field45" player2="" player3="" player4="" class="field"><div class="rotate">45</div></div>
+    <div id="field44" player2="" player3="" player4="" class="field"><div class="rotate">44</div></div>
+    <div id="field43" player2="" player3="" player4="" class="field"><div class="rotate">43</div></div>
+    <div id="field42" player2="" player3="" player4="" class="field field-security"><div class="rotate">42</div></div>
+    <div id="field41" player2="" player3="" player4="" class="field"><div class="rotate">41</div></div>
+    <div id="field40" player2="" player3="" player4="" class="field"><div class="rotate">40</div></div>
 </div>
 
 <div class="row-base row-second-player4">
-    <div id="field29" class="field">29</div>
-    <div id="field28" class="field">28</div>
-    <div id="field27" class="field">27</div>
-    <div id="field26" class="field">26</div>
-    <div id="field25" class="field field-security">25</div>
-    <div id="field24" class="field">24</div>
-    <div id="field23" class="field">23</div>
+    <div id="field29" player2="" player3="" player4="" class="field">29</div>
+    <div id="field28" player2="" player3="" player4="" class="field">28</div>
+    <div id="field27" player2="" player3="" player4="" class="field">27</div>
+    <div id="field26" player2="" player3="" player4="" class="field">26</div>
+    <div id="field25" player2="" player3="" player4="" class="field field-security">25</div>
+    <div id="field24" player2="" player3="" player4="" class="field">24</div>
+    <div id="field23" player2="" player3="" player4="" class="field">23</div>
 </div>
--- components/table/rowsStart.php
+++ components/table/rowsStart.php
@@ -3,37 +3,37 @@
     <div id="field66" class="field">66</div>
     <div id="field67" class="field">67</div>
     <div id="field68" class="field">68</div>
-    <div id="field1" class="field field-player1">1</div>
-    <div id="field2" class="field">2</div>
-    <div id="field3" class="field">3</div>
+    <div id="field1" player2="" player3="" player4="" class="field field-player1">1</div>
+    <div id="field2" player2="" player3="" player4="" class="field">2</div>
+    <div id="field3" player2="" player3="" player4="" class="field">3</div>
 </div>
 
 <div class="row-base row-base-player2">
-    <div id="field48" class="field"><div class="rotate">48</div></div>
-    <div id="field49" class="field"><div class="rotate">49</div></div>
-    <div id="field50" class="field"><div class="rotate">50</div></div>
-    <div id="field51" class="field"><div class="rotate">51</div></div>
-    <div id="field52" class="field field-player2"><div class="rotate">52</div></div>
-    <div id="field53" class="field"><div class="rotate">53</div></div>
-    <div id="field54" class="field"><div class="rotate">54</div></div>
+    <div id="field48" player2="" player3="" player4="" class="field"><div class="rotate">48</div></div>
+    <div id="field49" player2="" player3="" player4="" class="field"><div class="rotate">49</div></div>
+    <div id="field50" player2="" player3="" player4="" class="field"><div class="rotate">50</div></div>
+    <div id="field51" player2="" player3="" player4="" class="field"><div class="rotate">51</div></div>
+    <div id="field52" player2="" player3="" player4="" class="field field-player2"><div class="rotate">52</div></div>
+    <div id="field53" player2="" player3="" player4="" class="field"><div class="rotate">53</div></div>
+    <div id="field54" player2="" player3="" player4="" class="field"><div class="rotate">54</div></div>
 </div>
 
 <div class="row-base row-base-player3">
-    <div id="field31" class="field">31</div>
-    <div id="field32" class="field">32</div>
-    <div id="field33" class="field">33</div>
-    <div id="field34" class="field">34</div>
-    <div id="field35" class="field field-player3">35</div>
-    <div id="field36" class="field">36</div>
-    <div id="field37" class="field">37</div>
+    <div id="field31" player2="" player3="" player4="" class="field">31</div>
+    <div id="field32" player2="" player3="" player4="" class="field">32</div>
+    <div id="field33" player2="" player3="" player4="" class="field">33</div>
+    <div id="field34" player2="" player3="" player4="" class="field">34</div>
+    <div id="field35" player2="" player3="" player4="" class="field field-player3">35</div>
+    <div id="field36" player2="" player3="" player4="" class="field">36</div>
+    <div id="field37" player2="" player3="" player4="" class="field">37</div>
 </div>
 
 <div class="row-base row-base-player4">
-    <div id="field14" class="field"><div class="rotate-90">14</div></div>
-    <div id="field15" class="field"><div class="rotate-90">15</div></div>
-    <div id="field16" class="field"><div class="rotate-90">16</div></div>
-    <div id="field17" class="field"><div class="rotate-90">17</div></div>
-    <div id="field18" class="field field-player4"><div class="rotate-90">18</div></div>
-    <div id="field19" class="field"><div class="rotate-90">19</div></div>
-    <div id="field20" class="field"><div class="rotate-90">20</div></div>
+    <div id="field14" player2="" player3="" player4="" class="field"><div class="rotate-90">14</div></div>
+    <div id="field15" player2="" player3="" player4="" class="field"><div class="rotate-90">15</div></div>
+    <div id="field16" player2="" player3="" player4="" class="field"><div class="rotate-90">16</div></div>
+    <div id="field17" player2="" player3="" player4="" class="field"><div class="rotate-90">17</div></div>
+    <div id="field18" player2="" player3="" player4="" class="field field-player4"><div class="rotate-90">18</div></div>
+    <div id="field19" player2="" player3="" player4="" class="field"><div class="rotate-90">19</div></div>
+    <div id="field20" player2="" player3="" player4="" class="field"><div class="rotate-90">20</div></div>
 </div>
