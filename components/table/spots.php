8108b245cf54374ad1da12f4d740b66243b8c995
JuanesEsp1 <juanes.espinosa01@gmail.com>
2024-01-10 14:30:09 -0500

    JE ficahs dentro del spot

--- components/table/spots.php
+++ components/table/spots.php
@@ -1,23 +1,25 @@
 <div class="spot spot-player1">
     <div class="spot-container">
-
+        <div class="spot-miniContainer">
+            <div class="figurePlayer1"></div>
+        </div>
     </div>
 </div>
 
 <div class="spot spot-player2">
     <div class="spot-container">
-        
+        <div class="figurePlayer2"></div>
     </div>
 </div>
 
 <div class="spot spot-player3">
     <div class="spot-container">
-        
+        <div class="figurePlayer3"></div>
     </div>
 </div>
 
 <div class="spot spot-player4">
     <div class="spot-container">
-        
+        <div class="figurePlayer4"></div>
     </div>
 </div>
