4760ebfdfbf0ab189fabb751bc29e32e815c5832
Sebinhas <jsebascuervo05@gmail.com>
2024-01-10 19:46:09 -0500

    JS añadi los id

--- components/table/rowsSecond.php
+++ components/table/rowsSecond.php
@@ -1,39 +1,39 @@
 <div class="row-base row-second-player1">
-    <div player1="11" player2="28" player3="45" player4="62" class="field"><div class="rotate-90">12</div></div>
-    <div player1="10" player2="27" player3="44" player4="61" class="field"><div class="rotate-90">11</div></div>
-    <div player1="9" player2="26" player3="43" player4="60" class="field"><div class="rotate-90">10</div></div>
-    <div player1="8" player2="25" player3="42" player4="59" class="field"><div class="rotate-90">9</div></div>
-    <div player1="7" player2="24" player3="41" player4="58" class="field field-security"><div class="rotate-90">8</div></div>
-    <div player1="6" player2="23" player3="40" player4="57" class="field"><div class="rotate-90">7</div></div>
-    <div player1="5" player2="22" player3="39" player4="56" class="field"><div class="rotate-90">6</div></div>
+    <div id="field11" player1="11" player2="28" player3="45" player4="62" class="field"><div class="rotate-90">12</div></div>
+    <div id="field10" player1="10" player2="27" player3="44" player4="61" class="field"><div class="rotate-90">11</div></div>
+    <div id="field9" player1="9" player2="26" player3="43" player4="60" class="field"><div class="rotate-90">10</div></div>
+    <div id="field8" player1="8" player2="25" player3="42" player4="59" class="field"><div class="rotate-90">9</div></div>
+    <div id="field7" player1="7" player2="24" player3="41" player4="58" class="field field-security"><div class="rotate-90">8</div></div>
+    <div id="field6" player1="6" player2="23" player3="40" player4="57" class="field"><div class="rotate-90">7</div></div>
+    <div id="field5" player1="5" player2="22" player3="39" player4="56" class="field"><div class="rotate-90">6</div></div>
 </div>
 
 <div class="row-base row-second-player2">
-    <div player1="62" player2="11" player3="28" player4="45" class="field">63</div>
-    <div player1="61" player2="10" player3="27" player4="44" class="field">62</div>
-    <div player1="60" player2="9" player3="26" player4="43" class="field">61</div>
-    <div player1="59" player2="8" player3="25" player4="42" class="field">60</div>
-    <div player1="58" player2="7" player3="24" player4="41" class="field field-security">59</div>
-    <div player1="57" player2="6" player3="23" player4="40" class="field">58</div>
-    <div player1="56" player2="5" player3="22" player4="39" class="field">57</div>
+    <div id="field62" player1="62" player2="11" player3="28" player4="45" class="field">63</div>
+    <div id="field61"player1="61" player2="10" player3="27" player4="44" class="field">62</div>
+    <div id="field60"player1="60" player2="9" player3="26" player4="43" class="field">61</div>
+    <div id="field59"player1="59" player2="8" player3="25" player4="42" class="field">60</div>
+    <div id="field58"player1="58" player2="7" player3="24" player4="41" class="field field-security">59</div>
+    <div id="field57"player1="57" player2="6" player3="23" player4="40" class="field">58</div>
+    <div id="field56"player1="56" player2="5" player3="22" player4="39" class="field">57</div>
 </div>
 
 <div class="row-base row-second-player3">
-    <div player1="45" player2="62" player3="11" player4="28" class="field"><div class="rotate">46</div></div>
-    <div player1="44" player2="61" player3="10" player4="27" class="field"><div class="rotate">45</div></div>
-    <div player1="43" player2="60" player3="9" player4="26" class="field"><div class="rotate">44</div></div>
-    <div player1="42" player2="59" player3="8" player4="25" class="field"><div class="rotate">43</div></div>
-    <div player1="41" player2="58" player3="7" player4="24" class="field field-security"><div class="rotate">42</div></div>
-    <div player1="40" player2="57" player3="6" player4="23" class="field"><div class="rotate">41</div></div>
-    <div player1="39" player2="56" player3="5" player4="22" class="field"><div class="rotate">40</div></div>
+    <div id="field45"player1="45" player2="62" player3="11" player4="28" class="field"><div class="rotate">46</div></div>
+    <div id="field44"player1="44" player2="61" player3="10" player4="27" class="field"><div class="rotate">45</div></div>
+    <div id="field43"player1="43" player2="60" player3="9" player4="26" class="field"><div class="rotate">44</div></div>
+    <div id="field42"player1="42" player2="59" player3="8" player4="25" class="field"><div class="rotate">43</div></div>
+    <div id="field41"player1="41" player2="58" player3="7" player4="24" class="field field-security"><div class="rotate">42</div></div>
+    <div id="field40"player1="40" player2="57" player3="6" player4="23" class="field"><div class="rotate">41</div></div>
+    <div id="field39"player1="39" player2="56" player3="5" player4="22" class="field"><div class="rotate">40</div></div>
 </div>
 
 <div class="row-base row-second-player4">
-    <div player1="28" player2="45" player3="62" player4="11" class="field">29</div>
-    <div player1="27" player2="44" player3="61" player4="10" class="field">28</div>
-    <div player1="26" player2="43" player3="60" player4="9" class="field">27</div>
-    <div player1="25" player2="42" player3="59" player4="8" class="field">26</div>
-    <div player1="24" player2="41" player3="58" player4="7" class="field field-security">25</div>
-    <div player1="23" player2="40" player3="57" player4="6" class="field">24</div>
-    <div player1="22" player2="39" player3="56" player4="5" class="field">23</div>
+    <div id="field28"player1="28" player2="45" player3="62" player4="11" class="field">29</div>
+    <div id="field27"player1="27" player2="44" player3="61" player4="10" class="field">28</div>
+    <div id="field26"player1="26" player2="43" player3="60" player4="9" class="field">27</div>
+    <div id="field25"player1="25" player2="42" player3="59" player4="8" class="field">26</div>
+    <div id="field24"player1="24" player2="41" player3="58" player4="7" class="field field-security">25</div>
+    <div id="field23"player1="23" player2="40" player3="57" player4="6" class="field">24</div>
+    <div id="field22"player1="22" player2="39" player3="56" player4="5" class="field">23</div>
 </div>
